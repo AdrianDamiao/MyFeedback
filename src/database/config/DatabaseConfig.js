54b081dc523f5679566d5d20fb3b687221fcccf7
adriandamiaosoitic <79238503+AdrianDamiao@users.noreply.github.com>
2022-01-27 22:12:30 -0300

    Configura o Sequelize e o PostgresSQL

--- src/database/config/DatabaseConfig.js
+++ src/database/config/DatabaseConfig.js
@@ -1,14 +1,14 @@
 require('dotenv').config({
-    path: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env'
+    path: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env'
 });
 
 module.exports = {
-    dialect: 'postgres',
-    database: 'homolog',
-    host: 'localhost',
-    port: 4000,
-    username: 'admin',
-    password: '123qWe',
+    dialect: process.env.DATABASE_DIALECT,
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
     define: {
         timestamps: true,
         underscored: true
